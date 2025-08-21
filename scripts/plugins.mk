7e8c31b3ff2c3c3ee084839d481e22f361281e03
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2025-08-21 11:39:52 +0200

    fix(tfgen): add std to plugins for example generation

--- scripts/plugins.mk
+++ scripts/plugins.mk
@@ -4,6 +4,7 @@ install_plugins: .make/install_plugins
 .make/install_plugins: export PATH := $(WORKING_DIR)/.pulumi/bin:$(PATH)
 .make/install_plugins: .pulumi/bin/pulumi
 	.pulumi/bin/pulumi plugin install converter terraform 1.0.16
+	.pulumi/bin/pulumi plugin install resource std 2.2.0
 	@touch $@
 .PHONY: install_plugins
 
